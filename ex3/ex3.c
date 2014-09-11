2761709a908eb2f72117bd874fcf8d6cb17e7bd5
ahsanulkarim <ahsanulkarim@vm-0.ahsanulkarim.koding.kd.io>
2014-09-11 16:46:01 +0000

    ex3 added

---
+++ ex3/ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int age = 10;
+	int height = 72;
+	printf("age: %d years\n", age);
+	printf("height: %d inches\n", height);
+	return 0;
+}
